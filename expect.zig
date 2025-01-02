0c013852d12fe3efa51628f2938f32e93047a55a
Meghan Denny <hello@nektro.net>
2025-01-01 20:50:27 -0800

    support optionals

--- expect.zig
+++ expect.zig
@@ -9,15 +9,25 @@ pub fn Expect(T: type) type {
         actual: T,
         negate: bool = false,
 
+        const info = @typeInfo(T);
+
         pub fn toEqual(self: *const @This(), expected: T) !void {
             try std.testing.expectEqual(expected, self.actual);
         }
 
         pub fn toEqualSlice(self: *const @This(), expected: T) !void {
+            if (info == .Optional) {
+                try std.testing.expectEqualSlices(std.meta.Elem(T), expected, self.actual.?);
+                return;
+            }
             try std.testing.expectEqualSlices(std.meta.Elem(T), expected, self.actual);
         }
 
         pub fn toEqualString(self: *const @This(), expected: []const u8) !void {
+            if (info == .Optional) {
+                try std.testing.expectEqualStrings(expected, self.actual.?);
+                return;
+            }
             try std.testing.expectEqualStrings(expected, self.actual);
         }
 
@@ -29,6 +39,11 @@ pub fn Expect(T: type) type {
         }
 
         pub fn toBeNull(self: *const @This()) !void {
+            if (info == .Optional) {
+                if (!self.negate) try std.testing.expectEqual(@as(T, null), self.actual);
+                if (self.negate) try std.testing.expect(self.actual != null);
+                return;
+            }
             if (!self.negate) try std.testing.expectEqual(@as(?T, null), self.actual);
             if (self.negate) try std.testing.expect(self.actual != null);
         }
