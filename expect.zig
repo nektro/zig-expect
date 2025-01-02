25c0da90da515408034c723a909b86816ff96ff2
Meghan Denny <hello@nektro.net>
2025-01-01 20:49:44 -0800

    add toEqualSlice and toEqualString

--- expect.zig
+++ expect.zig
@@ -11,5 +11,13 @@ pub fn Expect(T: type) type {
         pub fn toEqual(self: *const @This(), expected: T) !void {
             try std.testing.expectEqual(expected, self.actual);
         }
+
+        pub fn toEqualSlice(self: *const @This(), expected: T) !void {
+            try std.testing.expectEqualSlices(std.meta.Elem(T), expected, self.actual);
+        }
+
+        pub fn toEqualString(self: *const @This(), expected: []const u8) !void {
+            try std.testing.expectEqualStrings(expected, self.actual);
+        }
     };
 }
